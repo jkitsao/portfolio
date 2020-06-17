d10a2035a4969eaff23c284019b5c70445723995
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-06-17 13:43:15 +0300

    adding tailwind and next js to the application....first commit

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import "../styles/tailwind.css";
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="p-4 shadow rounded bg-white">
+      <h1 className="text-purple-500 leading-normal">hello there world</h1>
+      <p className="text-gray-500">with Tailwind CSS</p>
+    </div>
+  );
+};
+export default App;

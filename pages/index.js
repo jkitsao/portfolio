9cb8db34d9cecbe4f3d40f8b926c0487a5cb065a
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-06-24 19:58:18 +0300

    create branch version1 and switch to version1

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
 import React from "react";
-import Panel from "../components/Panel";
-import TextInfo from "../components/TextInfo";
+
 export default function index() {
   return (
-    <div className="h-screen bg-white flex">
-      {/* left side links /panel */}
-      <div className="fixed">
-        <Panel />
-      </div>
-      {/* page content */}
-      <div className="bg-gray-200 w-full h-1/2 overflow-scroll">
-        <div className="p-40">
-          <TextInfo />
-        </div>
-      </div>
+    <div>
+      <h1>hello world</h1>
     </div>
   );
 }
